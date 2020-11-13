90521a0f82239fca851004ed5d0ec35acb5b817d
Bhaumik <sbhaumi@apac.corpdir.net>
2020-11-13 23:17:57 +0530

    Tick mark added

--- src/views/CircularProgress.tsx
+++ src/views/CircularProgress.tsx
@@ -9,15 +9,22 @@ const CircularProgress = (props: Props) => {
         <div
             className={`
             rounded-full
-            w-30px h-30px top-0 absolute
+            w-30px
+            h-30px
+            top-0
+            absolute
             ${props.isCompleted ? 'border-dodger_blue': 'border-greyish'}
-            
             ${props.isCompleted ? 'bg-dodger_blue': 'bg-greyish'}
+            text-center
             `}
             style={{
                 left: '-35px'
             }}
-        />
+        >
+            {props.isCompleted &&
+			    <span className='text-white'>&#10003;</span>
+            }
+        </div>
     );
 }
 
